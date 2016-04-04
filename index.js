f937fcc6ee3e476c665ffc6cba101aad8d8471e0
oredds <oliver.jk.redding@gmail.com>
2016-04-04 12:04:39 +1200

    Updates

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 var xhr = require('xhr')
 var example = require('./views/example.hbs')
+// var request = require('request')
 
-xhr.get('https://api.wheretheiss.at/v1/satellites', function(err, data) {
-  if (err) console.log(err) // do something
-
-  console.log(data.body)
-  document.body.innerHTML = example({ name: "Space" });
+// xhr.setRequestHeader( 'Api-User-Agent', 'Example/1.0' );
+// xhr.get('https://api.wheretheiss.at/v1/satellites', function(err, data) {
+xhr.get({
+    uri: 'https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json',
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    }
+  }, function(err, data) {
+// request.get('https://api.wheretheiss.at/v1/satellites', function(err, data) {
+  if(err) {console.log(err); return}
+  console.log(JSON.parse(data.body).query.pages['15580374'].title)
+  var msg = JSON.parse(data.body).query.pages['15580374'].title
+  document.body.innerHTML = example(msg);
 })
+
+
 
