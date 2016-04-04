b3a3d3527ec39da814e57ebc5d39471c2c090a3a
oredds <oliver.jk.redding@gmail.com>
2016-04-04 12:39:17 +1200

    Updates

--- index.js
+++ index.js
@@ -4,16 +4,24 @@ var example = require('./views/example.hbs')
 
 // xhr.setRequestHeader( 'Api-User-Agent', 'Example/1.0' );
 // xhr.get('https://api.wheretheiss.at/v1/satellites', function(err, data) {
-xhr.get({
-    uri: 'https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json',
-    headers: {
-      'Access-Control-Allow-Origin': '*'
-    }
-  }, function(err, data) {
+// var client = new XMLHttpRequest();
+// client.open('GET', 'https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json');
+// client.setRequestHeader('Access-Control-Allow-Origin', '*');
+// client.send();
+
+
+// xhr.get({
+//     uri: 'https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json',
+//     headers: {
+//       'Access-Control-Allow-Origin': '*'
+//     }
+//   }, function(err, data) {
+
+xhr.get('~/Workspace/meowtown/meowtown/app.js', function(err, data) {
 // request.get('https://api.wheretheiss.at/v1/satellites', function(err, data) {
   if(err) {console.log(err); return}
-  console.log(JSON.parse(data.body).query.pages['15580374'].title)
-  var msg = JSON.parse(data.body).query.pages['15580374'].title
+  console.log(data.body)
+  var msg = JSON.parse(data.body)
   document.body.innerHTML = example(msg);
 })
 
